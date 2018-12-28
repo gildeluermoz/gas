5e51c1d9ef5dbd2ce21d3c35a18afe4b69007bf4
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-12-28 22:35:25 +0100

    Formulaire produit - commencer par choisir la livraison

--- app/t_products/forms.py
+++ app/t_products/forms.py
@@ -14,7 +14,7 @@ class Product(FlaskForm):
     active = BooleanField('Actif', default = True, false_values=(False, 'false'))
     product_name = StringField('Nom', validators=[DataRequired(message = 'Le nom du produit est obligatoire')])
     product_comment = TextAreaField('Commentaire')
-    id_delivery = SelectField('Livraison', coerce=str, choices=[], default='0')
+    id_delivery = SelectField('Choisir la livraison pour ce produit', coerce=str, choices=[], default='0')
     buying_price = DecimalField("Prix d'achat")
     selling_price = DecimalField("Prix de vente")
     case_weight = IntegerField('Poids de la caisse')
