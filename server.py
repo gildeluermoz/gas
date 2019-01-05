395099d4063fb8dcad6dc196fc0b162e6c55a43b
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2019-01-05 22:35:09 +0100

    nettoyage

--- server.py
+++ server.py
@@ -100,9 +100,6 @@ with app.app_context():
         from app.t_orders import route
         app.register_blueprint(route.route, url_prefix='/')
 
-        # from app.auth import route
-        # app.register_blueprint(route.route, url_prefix='/log')
-
         from app.auth import route
         app.register_blueprint(route.route, url_prefix='/login')
 
