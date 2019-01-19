2c86db896637adc0cce5ae184280afc0a37d2970
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2019-01-19 17:32:35 +0100

    nettoyage

--- app/login/route.py
+++ app/login/route.py
@@ -1,16 +1,6 @@
 from flask import (
-    Flask, redirect, render_template,
-    Blueprint, request, session, flash
+    Flask, render_template, Blueprint, flash
 )
-from app import genericRepository
-from app.login import forms as authforms
-from app.models import TUsers
-from app.utils.utilssqlalchemy import json_resp
-from app.env import db
-from flask_bcrypt import (Bcrypt,
-                          check_password_hash,
-                          generate_password_hash)
-import bcrypt
 
 route = Blueprint('login', __name__)
 
